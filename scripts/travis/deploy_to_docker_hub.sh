b47a47fcadb53d8b71a6f992cbd1fde400bbd044
Anthony <anthony.denecheau@gmail.com>
2019-02-10 19:15:39 +0100

    dockerfile

--- scripts/travis/deploy_to_docker_hub.sh
+++ scripts/travis/deploy_to_docker_hub.sh
@@ -1,3 +1,4 @@
 echo "Pushing service docker images to docker hub ...."
+ls -la
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push anthonydenecheau/gopocservice:$BUILD_NAME
